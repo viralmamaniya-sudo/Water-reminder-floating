6b9f30ce9e62bccba18ba6baef361c0a78b0221a
viralmamaniya-sudo <viralmamaniya@gmail.com>
2026-07-13 14:21:22 -0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.viral.waterreminder"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.viral.waterreminder"
         minSdk = 26
@@ -12,8 +14,13 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-}
-dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
